61f48142992a6f8f78dee02bb0061d984044351f
Michael DeHaan <michael@reductivelabs.com>
2010-03-31 14:49:09 -0400

    Add a shell script that packages up the docs.

---
+++ gentar.sh
@@ -0,0 +1,8 @@
+now=`date '+%m-%d-%y'`
+filename="puppetdocs-$now.tar"
+(rm $filename)
+(rm $filename.gz)
+rake generate
+(cd output && tar -cvf ../$filename *)
+gzip $filename
+cp $filename.gz puppetdocs-latest.gz
