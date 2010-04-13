b6e11947ebb22c94650144cd65a09c8f3eea9dc6
Michael DeHaan <michael@reductivelabs.com>
2010-04-13 11:47:32 -0400

    Move to using relative paths and construct docsite build to include files in the correct places.  For now this means for testing if you should use "gentar.sh" and "firefox ." rather than "rake run" as that will not copy over the duplicate files.

--- gentar.sh
+++ gentar.sh
@@ -1,8 +1,17 @@
+rake generate
+# very lame hack to make docs usable via relative paths
+# for downloaded copies, which requires that HTML
+# versions also have relative paths.  
+cp -r output/files output/guides/files
+cp -r output/files output/guides/types/files
+cp -r output/files output/guides/types/nagios/files
+cp -r output/files output/guides/types/selinux/files
+cp -r output/files output/guides/types/ssh/files
+cp -r output/files output/references/files
 now=`date '+%m-%d-%y'`
 filename="puppetdocs-$now.tar"
 (rm $filename)
 (rm $filename.gz)
-rake generate
 (cd output && tar -cvf ../$filename *)
 gzip $filename
 cp $filename.gz puppetdocs-latest.tar.gz
