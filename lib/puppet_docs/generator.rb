f30982835c43fc0feec4a686057e8eb8c423a468
James Turnbull <james@lovedthanlost.net>
2010-05-09 12:31:33 +1000

    Fixed Reference headers and indexes to be cleaner

--- lib/puppet_docs/generator.rb
+++ lib/puppet_docs/generator.rb
@@ -150,10 +150,11 @@ module PuppetDocs
     end
 
     def set_ref_header_section(name, body, view)
-      header, new_body = body.split(/\*\(last generated on .*\)\*/, 2)
+      header, new_body = body.split(/\*\*This page .*\*\*$|\*\(last generated .*\)\*$/, 2)
       if new_body
         if header =~ /^\%\s(\S[^\r\n]+)\r?\n\%\s*$/ms
           page_title = $1.strip
+          header = ''
         else
           return false
         end 
