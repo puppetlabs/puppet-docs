b4904b332c58f924c35ffa562072fefe6765755b
Nick Fagerlund <nick.fagerlund@gmail.com>
2013-12-03 14:46:37 -0800

    Write mixture of processed and unprocessed man pages
    Processing applications, not processing faces. Gonna unify these next.

--- lib/puppet_docs/reference/man.rb
+++ lib/puppet_docs/reference/man.rb
@@ -12,7 +12,7 @@ module PuppetDocs
         applications = Dir.glob(%Q{#{puppet_dir}/lib/puppet/application/*}).collect{ |app_file|
           app_file.sub(/^#{puppet_dir}\/lib\/puppet\/application\/([\w_]+)\.rb$/, '\1')
         }
-        require 'puppet/face'
+        require 'puppet/face' # works because rubylib is already munged???
         # All four lines below are terrible. See puppet/tasks/manpages.rake for details.
         Puppet.initialize_settings()
         helpface = Puppet::Face[:help, '0.0.1']
@@ -23,23 +23,30 @@ module PuppetDocs
         faces = Puppet::Face.faces
         faces.delete(:resource)
 
-
-
-
+        # Dump the application names to a file
         Dir.chdir(destination_dir) do
           File.open("./this_would_be_the_man_page.txt", 'w') {|f| f.puts non_face_applications.join(', '); f.puts faces.join(', ')}
         end
 
-#         ronn = 'bundle exec ronn'
-#         applications.each do |app|
-#           headerstring = "---\nlayout: default\ntitle: puppet #{app} Manual Page\n---\n\n"
-#           # RUBYLIB is already cleaned out and initialized in the Generator#generate method.
-#           manstring = %x{ruby #{puppet_dir}/bin/puppet #{app} --help | #{ronn} --pipe -f}
-#           # `ruby #{puppet_dir}/bin/puppet doc --modulepath /tmp/nothing --libdir /tmp/alsonothing -m text -r #{@name}`
-#           File.open("#{destination_dir}/#{app}.markdown", 'w') do |file|
-#             file.puts("#{headerstring}#{manstring}")
-#           end
-#         end
+        man_strings = {}
+
+
+        # Generate and cache all man strings (raw with no header, etc.)
+        non_face_applications.each do |app|
+          # RUBYLIB is already cleaned out and initialized in the Generator#generate method.
+          man_strings[app] = %x{ruby #{puppet_dir}/bin/puppet #{app} --help | bundle exec ronn --pipe -f}
+        end
+        faces.each do |face|
+          man_strings[face] = manface.man("#{face}")
+        end
+
+        # Write files
+        man_strings.each do |name, man_string|
+          headerstring = "---\nlayout: default\ntitle: puppet #{name} Manual Page\n---\n\n"
+          File.open("#{destination_dir}/#{name}.markdown", 'w') do |file|
+            file.puts("#{headerstring}#{man_string}")
+          end
+        end
 
       end # self.write_manpages
     end
