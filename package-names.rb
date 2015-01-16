1acba6afec33c78a03fcc05859e7c17b13b219f4
Nick Fagerlund <nick.fagerlund@gmail.com>
2015-01-16 13:15:28 -0800

    Add file mapping social names to package names, for normalizing.

---
+++ package-names.rb
@@ -0,0 +1,151 @@
+puppet
+  <td>pe-agent</td>
+  <td>pe-puppet</td>
+  <td>pup-puppet</td>
+  <td>pe-puppetserver-common</td>
+
+facter
+  <td>pe-facter</td>
+  <td>pup-facter</td>
+
+
+hiera
+  <td>pe-hiera</td>
+  <td>pup-hiera</td>
+
+
+
+mcollective
+  <td>pe-mcollective</td>
+  <td>pe-mcollective-client</td>
+  <td>pe-mcollective-common</td>
+  <td>pup-mcollective</td>
+
+NXOS agent
+  <td>puppet-enterprise-nxos-1-i386</td>
+  <td>puppet-enterprise-nxos-1-x86_64</td>
+
+
+PuppetDB
+  <td>pe-puppetdb</td>
+  <td>pe-puppetdb-terminus</td>
+
+Puppet Server
+  <td>pe-puppetserver</td>
+
+Razor server
+  <td>pe-razor-server</td>
+
+razor libs
+  <td>pe-razor-libs</td>
+
+
+Ruby
+  <td>pe-ruby</td>
+  <td>pup-ruby</td>
+
+apache
+  <td>pe-httpd</td>
+  <td>pe-httpd-mpm-event</td>
+  <td>pe-httpd-mpm-itk</td>
+  <td>pe-httpd-mpm-prefork</td>
+  <td>pe-httpd-mpm-worker</td>
+  <td>pe-httpd-prefork-dev</td>
+  <td>pe-httpd-threaded-dev</td>
+
+ActiveMQ
+  <td>pe-activemq</td>
+
+
+PostgreSQL
+  <td>pe-postgresql</td>
+  <td>pe-postgresql-contrib</td>
+  <td>pe-postgresql-devel</td>
+  <td>pe-postgresql-server</td>
+
+
+Passenger
+  <td>pe-passenger</td>
+
+MySQL
+
+
+OpenSSL
+  <td>pe-openssl</td>
+  <td>pup-openssl</td>
+
+Java
+  <td>pe-java</td>
+
+LibAPR (seems to be just EL and SLES)
+  <td>pe-libapr</td>
+  <td>pe-libaprutil</td>
+
+Misc libs and ruby gems
+  <td>pe-ruby-augeas</td>
+  <td>pe-ruby-ldap</td>
+  <td>pe-ruby-mysql</td>
+  <td>pe-ruby-rgen</td>
+  <td>pe-ruby-selinux</td>
+  <td>pe-ruby-shadow</td>
+  <td>pe-ruby-stomp</td>
+  <td>pe-rubygem-activerecord</td>
+  <td>pe-rubygem-activesupport</td>
+  <td>pe-rubygem-deep-merge</td>
+  <td>pe-rubygem-gem2rpm</td>
+  <td>pe-rubygem-net-http-unix</td>
+  <td>pe-rubygem-net-ssh</td>
+  <td>pe-rubygem-rack</td>
+  <td>pe-rubygem-sequel</td>
+  <td>pe-rubygem-sinatra</td>
+  <td>pe-rubygem-stomp</td>
+  <td>pup-ruby-augeas</td>
+  <td>pup-ruby-rgen</td>
+  <td>pup-ruby-shadow</td>
+  <td>pe-libevent</td>
+  <td>pe-libevent-devel</td>
+  <td>pe-libldap</td>
+  <td>pe-libyaml</td>
+  <td>pe-virt-what</td>
+  <td>pup-deep-merge</td>
+  <td>pup-libyaml</td>
+  <td>pe-deep-merge</td>
+  <td>pe-cfpropertylist</td>
+  <td>pe-bundler</td>
+  <td>pe-activerecord</td>
+  <td>pe-activesupport</td>
+
+Misc other
+  <td>pe-cloud-provisioner</td>
+  <td>pe-cloud-provisioner-libs</td>
+  <td>pe-installer</td>
+  <td>pe-license</td>
+  <td>pe-license-status</td>
+  <td>pe-puppet-enterprise-release</td>
+  <td>pe-puppet-license-cli</td>
+  <td>pup-puppet-enterprise-release</td>
+
+
+Augeas
+  <td>pe-augeas</td>
+  <td>pup-augeas</td>
+
+stomp
+  <td>pe-stomp</td>
+  <td>pup-stomp</td>
+
+
+console stuff
+  <td>pe-certificate-manager</td>
+  <td>pe-certificate-manager-test</td>
+  <td>pe-console</td>
+  <td>pe-console-auth</td>
+  <td>pe-console-services</td>
+  <td>pe-console-services-termini</td>
+  <td>pe-console-test</td>
+  <td>pe-event-inspector</td>
+  <td>pe-live-management</td>
+  <td>pe-memcached</td>
+  <td>pe-memcached-devel</td>
+  <td>pe-puppet-dashboard</td>
+
