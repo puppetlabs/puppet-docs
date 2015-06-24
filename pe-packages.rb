fc5daae3afb4a160234b3375c4508a763c3ab42f
Nick Fagerlund <nick.fagerlund@gmail.com>
2015-06-24 14:23:33 -0700

    Hack together a big html table

--- pe-packages.rb
+++ pe-packages.rb
@@ -3,6 +3,7 @@
 require 'json'
 require 'pp'
 
+# this is like { platformname: { packagename: { version: version, md5: md5 }, packagename: {...} }, platformname: {......} }
 def load_package_json(version)
   Dir.chdir(File.expand_path('~/Documents/misc_code/enterprise-dist')) do
     system('git fetch upstream')
@@ -93,25 +94,27 @@ end
 
 }
 
-versions_of_interest = [
+versions_of_interest_old = [
 '3.2.0',
 '3.2.1',
 '3.2.2',
 '3.2.3',
 '3.3.0',
 '3.3.1',
-'3.3.2',
-'3.4.0',
+'3.3.2'
+]
+
+versions_of_interest_new = [
 '3.7.0',
 '3.7.1',
 '3.7.2',
-'3.7.3',
 '3.8.0',
 '3.8.1'
 ]
 
+versions_of_interest = versions_of_interest_new
 
-def normalize_package_data_for_version(packagedata)
+def normalize_package_data(packagedata)
   result = {}
   packagedata.each do | platform, platform_hash |
     platform_hash.each do | package_name, package_data |
@@ -129,11 +132,40 @@ def normalize_package_data_for_version(packagedata)
   result
 end
 
+historical_packages = versions_of_interest.reduce( {} ) do |result, version|
+  result[version] = normalize_package_data( load_package_json(version) )
+  result
+end
 
-packagedata = load_package_json('3.7.1')
-# this is like { platformname: { packagename: { version: version, md5: md5 }, packagename: {...} }, platformname: {......} }
+# now make a table
 
-pe371 = normalize_package_data_for_version(packagedata)
+print <<EOT
+<table>
+  <thead>
+    <tr>
+      <th>Pkg. / Ver.</th>
+      <th>#{versions_of_interest.join('</th> <th>')}</th>
+    </tr>
+  </thead>
 
-pp pe371
+  <tbody>
+    <tr>
+      #{
+        @package_name_variations.keys.map{ |package| # each row
+          "<td>" + versions_of_interest.map{ |version|
+            if historical_packages[version][package]
+              historical_packages[version][package].map{ |packageversion, oses|
+                '<abbr title="' + oses.join(", ") + '">' + packageversion + '</abbr>'
+              }.join("<br>")
+            else
+              ""
+            end
+          }.unshift(package).join("</td> <td>") + "</td>"
+        }.join('</tr> <tr>')
+      }
+    </tr>
+  </tbody>
+</table>
+EOT
 
+# done
