cd3c89499af7d1a40c4aed4fcbf87a0598a197a4
Nick Fagerlund <nick.fagerlund@gmail.com>
2015-07-17 16:19:11 -0700

    It outputs a table!

---
+++ agent_packages.rb
@@ -0,0 +1,53 @@
+#!/usr/bin/env ruby
+
+require 'git'
+require 'pathname'
+require 'json'
+require 'pp'
+
+agent_packages_dir = Pathname.new(File.expand_path(__FILE__)).parent
+agent_repo = Git.open(agent_packages_dir + 'puppet-agent')
+
+component_files = {
+  'Puppet' => 'puppet.json',
+  'Facter' => 'facter.json',
+  'Hiera' => 'hiera.json',
+  'MCollective' => 'marionette-collective.json'
+}
+
+tags = agent_repo.tags
+# Structure of the repo didn't stabilize until 0.9.0-ish, so:
+until tags.first.name == '1.0.0'
+  tags.shift
+end
+
+version_info = tags.map {|tag|
+  agent_repo.checkout(tag)
+  versions = component_files.values.map {|json|
+    # We want the last component of a string like refs/tags/4.2.0.
+    component_file = agent_packages_dir + 'puppet-agent/configs/components' + json
+    JSON.load(component_file.read)['ref'].split('/')[-1]
+  }
+  [tag.name, versions].flatten
+}
+
+version_header = ['puppet-agent'].concat(component_files.keys)
+# version_table = version_info.unshift(version_header)
+
+html_rows = version_info.map {|r|
+  "<td>" << r.join("</td> <td>") << "</td>"
+}
+
+html_table = <<END
+<table>
+  <thead>
+    <tr><th>#{version_header.join('</th> <th>')}</th></tr>
+  </thead>
+  <tbody>
+    <tr>#{html_rows.join("</tr>\n    <tr>")}</tr>
+  </tbody>
+</table>
+
+END
+
+puts html_table
