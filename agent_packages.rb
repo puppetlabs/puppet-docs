9360b339efd674d317a2d8f1655310aef27f565a
Nick Fagerlund <nick.fagerlund@gmail.com>
2015-07-29 15:05:02 -0700

    Comment stuff out

--- agent_packages.rb
+++ agent_packages.rb
@@ -22,15 +22,15 @@ until tags.first.name == '1.0.0'
   tags.shift
 end
 
-version_info = tags.map {|tag|
-  agent_repo.checkout(tag)
-  versions = component_files.values.map {|json|
-    # We want the last component of a string like refs/tags/4.2.0.
-    component_file = agent_packages_dir + 'puppet-agent/configs/components' + json
-    JSON.load(component_file.read)['ref'].split('/')[-1]
-  }
-  [tag.name, versions].flatten
-}
+# version_info = tags.map {|tag|
+#   agent_repo.checkout(tag)
+#   versions = component_files.values.map {|json|
+#     # We want the last component of a string like refs/tags/4.2.0.
+#     component_file = agent_packages_dir + 'puppet-agent/configs/components' + json
+#     JSON.load(component_file.read)['ref'].split('/')[-1]
+#   }
+#   [tag.name, versions].flatten
+# }
 
 version_info_hash = tags.reduce(Hash.new) {|result, tag|
   agent_repo.checkout(tag)
@@ -44,24 +44,24 @@ version_info_hash = tags.reduce(Hash.new) {|result, tag|
   result
 }
 
-version_header = ['puppet-agent'].concat(component_files.keys)
-# version_table = version_info.unshift(version_header)
-
-html_rows = version_info.map {|r|
-  "<td>" << r.join("</td> <td>") << "</td>"
-}
-
-html_table = <<END
-<table>
-  <thead>
-    <tr><th>#{version_header.join('</th> <th>')}</th></tr>
-  </thead>
-  <tbody>
-    <tr>#{html_rows.join("</tr>\n    <tr>")}</tr>
-  </tbody>
-</table>
-
-END
+# version_header = ['puppet-agent'].concat(component_files.keys)
+# # version_table = version_info.unshift(version_header)
+#
+# html_rows = version_info.map {|r|
+#   "<td>" << r.join("</td> <td>") << "</td>"
+# }
+#
+# html_table = <<END
+# <table>
+#   <thead>
+#     <tr><th>#{version_header.join('</th> <th>')}</th></tr>
+#   </thead>
+#   <tbody>
+#     <tr>#{html_rows.join("</tr>\n    <tr>")}</tr>
+#   </tbody>
+# </table>
+#
+# END
 
 # puts html_table
 puts JSON.dump(version_info_hash)
