84dac8d7b777a9f68abdcd442c9c2e973f6e9ca4
Jorie Tappa <jorie@jorietappa.com>
2015-12-10 17:35:02 -0800

    Removing everything in /source/learning, as it is all being redirected elsewhere.

--- source/learning/files/examples/answers/variables-build.pp
+++
@@ -1,8 +0,0 @@
-if $operatingsystem =~ /(?i:debian|ubuntu)/ {
-  package {'build-essential':
-    ensure => installed,
-  }
-}
-package {'gcc':
-  ensure => installed,
-}
--- source/learning/files/learningpuppet/tests/makeagent.pp
+++
@@ -1,3 +0,0 @@
-class {'learningpuppet::makeagent':
-  newname => 'agent3',
-}
