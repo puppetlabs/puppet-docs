96b14288403cbce5bfb72904498029b87091f024
Nick Fagerlund <nick.fagerlund@gmail.com>
2015-12-15 20:22:58 -0800

    You can't reference an instance variable from a constant! Geez!

--- lib/puppet_references/reference.rb
+++ lib/puppet_references/reference.rb
@@ -3,14 +3,14 @@ require 'puppet_references'
 module PuppetReferences
   class Reference
     attr_accessor :commit
-    DEFAULT_HEADER_DATA = {layout: 'default',
-                           built_from_commit: @commit}
     def initialize(commit)
       @commit = commit
     end
 
     def make_header(header_data)
-      PuppetReferences::Util.make_header(DEFAULT_HEADER_DATA.merge(header_data))
+      default_header_data = {layout: 'default',
+                             built_from_commit: @commit}
+      PuppetReferences::Util.make_header(default_header_data.merge(header_data))
     end
   end
 end
