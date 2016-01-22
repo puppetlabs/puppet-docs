10d9cd27fa4427602344b69f5726ae83aa24233f
Nick Fagerlund <nick.fagerlund@gmail.com>
2016-01-22 16:50:08 +0100

    (DOC-2480) Revise page doc info generator to use pre-munged data
    Now that we're doing all our _config.yml munging in one place, kick the munging
    functions out of the generator plugin that attaches doc info to pages. Cleaner!

--- lib/puppet_docs/config.rb
+++ lib/puppet_docs/config.rb
@@ -1,4 +1,5 @@
 require 'yaml'
+require 'pathname'
 
 module PuppetDocs
   class Config < Hash
@@ -8,11 +9,30 @@ module PuppetDocs
 
       # Merge document info into external sources. Expected behavior is that documents override
       # standalone sources if there's a conflict.
-      self['documents'].each {|url, info|
-        if info['external_source']
-          self['externalsources'][url] = info['external_source']
+      self['documents'].each {|base_url, data|
+        if data['external_source']
+          self['externalsources'][base_url] = data['external_source']
         end
       }
+
+      # Expand the document data: add a base_url key and expand the nav path
+      self['documents'].each {|base_url, data|
+        data['base_url'] = base_url
+        data['nav'] = (Pathname.new(base_url) + data['nav']).to_s
+      }
+
+      # Summarize the document data into a hierarchical hash with all the versions per document, with each
+      # version mapping to the base URL you'd use to find that document's data in the main documents hash.
+      # Like:
+      # {'pe' => {'2015.3' => '/pe/2015.3', '3.8' => '/pe/3.8'}, puppet => { ... } }
+      self['document_list'] = self['documents'].reduce( {} ) {|memo, (base_url, data)|
+        memo[data['doc']] ||= {}
+        memo[data['doc']][data['version']] = base_url
+
+        memo
+      }
+
+
     end
 
   end
--- source/_plugins/investigate_generator.rb
+++ source/_plugins/investigate_generator.rb
@@ -6,20 +6,17 @@ require 'byebug'
 
 module Jekyll
   class InvestigateGenerator < Generator
-    priority :lowest
+    # priority :lowest
 
     def generate(site)
-      @all_docs = {}
       puts 'about to do the thing'
       puts Time.now.to_s
       byebug
 
       documents = site.config['documents']
-      docs_for_pages = prep_doc_data_for_pages(documents)
-      site.config['document_list'] = collate_doc_data(documents)
 
       site.pages.each do |page|
-        (baseurl, data) = doc_for_page(page.url, docs_for_pages)
+        (base_url, data) = doc_for_page(page.url, documents)
         next if data.nil?
         page.data['doc'] = data
         page.data['nav'] = data['nav']
@@ -32,32 +29,10 @@ module Jekyll
 
     end
 
-    # Takes a config documents hash, returns a slightly different documents hash with
-    # some data duplicated or massaged.
-    def prep_doc_data_for_pages(documents)
-      documents.reduce( {} ) {|memo, (base_url, data)|
-        memo[base_url] = {}
-        memo[base_url]['base_url'] = base_url
-        memo[base_url]['doc'] = data['doc']
-        memo[base_url]['version'] = data['version']
-        memo[base_url]['nav'] = (Pathname.new(base_url) + data['nav']).to_s
-        memo[base_url]['external_source'] = data['external_source']
-        memo[base_url]['my_versions'] = data['my_versions']
-
-        memo
-      }
-    end
-
-    # Takes a config documents hash, and returns a hierarchical hash with all the versions per document, showing
-    # how to find each version in the main documents hash. Like:
-    # {'pe' => {'2015.3' => '/pe/2015.3', '3.8' => '/pe/3.8'}, puppet => { ... } }
-    def collate_doc_data(documents)
-      documents.reduce( {} ) {|memo, (base_url, data)|
-        memo[data['doc']] ||= {}
-        memo[data['doc']][data['version']] = base_url
-
-        memo
-      }
+    # Takes a page URL and a whole documents hash.
+    # Returns a two-element array [doc_baseurl, doc_data].
+    def doc_for_page(page_url, documents)
+      documents.detect {|url, data| Regexp.new("^#{url}") =~ page_url}
     end
 
     # Original:
@@ -100,11 +75,6 @@ module Jekyll
     # puppet:
     #   4.3: /puppet/4.3/reference
 
-    # Takes a page URL and a whole documents hash.
-    # Returns a two-element array [doc_baseurl, doc_data].
-    def doc_for_page(page_url, documents)
-      documents.detect {|url, data| Regexp.new("^#{url}") =~ page_url}
-    end
   end
 end
 
