d586ee2c164d4ad0524ac1787c7d9533939468db
Nick Fagerlund <nick.fagerlund@gmail.com>
2016-02-05 16:45:32 -0800

    (guts) Set shortcut variables for linking to documents, like {{puppet}}, etc.
    With this commit, you can link to the "locally correct" version of a given
    document with something like...
    
    [classes]: {{puppet}}/lang_classes.html
    
    If you're linking from a versioned document that knows about its own version of
    the destination document (via _config.yml), this will resolve to the correct
    version. If not, it falls back to the latest version.

--- source/_plugins/document_data_hook.rb
+++ source/_plugins/document_data_hook.rb
@@ -11,5 +11,21 @@ Jekyll::Hooks.register :pages, :pre_render do |page, payload|
     payload['page']['nav'] = data['nav']
   end
 
+  # Set special shortcut variables ( {{puppet}}, {{pe}}, etc.) for linking to our main documents.
+  # These resolve to the *base URL* of the "correct" version of a given document. So for example,
+  # {{puppet}} would render as /puppet/4.3/reference, or whatever.
+  # (This means we can't ever release a product called "page" or "site," but I'm really not concerned about that???)
+  document_version_index = payload['site']['document_version_index']
+  if data
+    # If we're coming from a versioned doc, link to our known version. (Defaults to latest if no one claimed anyone.)
+    data['my_versions'].each do |doc, version|
+      payload[doc] = document_version_index[doc][version]
+    end
+  else
+    # Fall back to latest if this isn't a versioned doc.
+    document_version_index.each do |doc, versions|
+      payload[doc] = versions['latest']
+    end
+  end
 
 end
