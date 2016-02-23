740f1f02e77bae16711ec5f9c5f57df23a8bde49
Daniel Parks <dp@puppetlabs.com>
2016-02-22 19:01:46 -0800

    (guts) Combine old swap_nginx_conf into install.sh.

--- source/install.sh
+++ source/install.sh
@@ -31,5 +31,26 @@ fi
 mv "$final.moving" "$stage"
 
 # Swap NGINX configuration into place and reload NGINX
-/opt/operations/scripts/swap_nginx_conf \
-  "$final/nginx_rewrite.conf" "$root/nginx_rewrite.conf"
+source_conf="$final/nginx_rewrite.conf"
+target_conf="$root/nginx_rewrite.conf"
+
+if diff -q "$source_conf" "$target_conf" >/dev/null 2>&1 ; then
+  # Files are the same; no need to swap or reload
+  exit 0
+fi
+
+cp "$target_conf" "$target_conf".good
+cp "$source_conf" "$target_conf"
+sudo service nginx configtest || {
+  # cp instead of mv in case we can't write to the directory
+  cp "$target_conf".good "$target_conf"
+  echo "$0: '$source_conf' not valid" >&2
+  exit 1
+}
+
+sudo service nginx reload || {
+  # cp instead of mv in case we can't write to the directory
+  cp "$target_conf".good "$target_conf"
+  echo "$0: '$source_conf' tested valid, but NGINX cannot reload" >&2
+  exit 1
+}
