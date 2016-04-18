8e3df49d0797bc974d1917dd23bff32425010833
Nick Fagerlund <nick.fagerlund@gmail.com>
2016-04-18 11:37:11 -0700

    Delete ancient website trash
    This commit also removes syntax.css (a color scheme for Pygments / Rouge) -- not
    precisely trash, but we don't currently use it and it's out-of-step with our
    current design. If we decide to switch to Rouge at some point, we can grab a new
    color scheme we actually like.

--- source/files/javascripts/html5.js
+++
@@ -1,3 +0,0 @@
-// Apparently this is for old IE stuff? http://msdn.microsoft.com/en-us/library/ie/8ka90k2e%28v=vs.94%29.aspx I don't know if we can get away with deleting it, but given that it's not 2010 anymore, I'm going to say "probably." -NF
-// For discussion and comments, see: http://remysharp.com/2009/01/07/html5-enabling-script/ (credit to @jdalton for minif)
-/*@cc_on'abbr article aside audio canvas details figcaption figure footer header hgroup mark menu meter nav output progress section summary time video'.replace(/\w+/g,function(n){document.createElement(n)})@*/
