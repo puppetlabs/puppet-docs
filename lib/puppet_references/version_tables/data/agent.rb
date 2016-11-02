9b87be10489b187e45a5fa4213f323d25ad3f4d0
Nick Fagerlund <nick.fagerlund@gmail.com>
2016-11-02 12:43:46 -0700

    (guts) Stopgap fix: point agent 1.8.0+ to the new ruby component file
    This isn't sustainable, because the file will keep moving. Luckily, we
    finally have a fix for RE-5150! Updated code incoming.

--- lib/puppet_references/version_tables/data/agent.rb
+++ lib/puppet_references/version_tables/data/agent.rb
@@ -1,6 +1,7 @@
 require 'puppet_references'
 require 'pathname'
 require 'json'
+require 'versionomy'
 
 module PuppetReferences
   module VersionTables
@@ -47,22 +48,46 @@ module PuppetReferences
                 @versions_and_commits.map {|name, commit|
                   puts "#{name}..."
                   @repo.checkout(commit)
-                  components_hash = @component_files.reduce(Hash.new) {|result, (component, config)|
-                    component_file = PuppetReferences::AGENT_DIR + 'configs/components' + config
-                    if component_file.extname == '.json'
-                      result[component] = version_from_json(component_file)
-                    elsif component_file.extname == '.rb'
-                      result[component] = version_from_ruby(component_file)
-                    else
-                      raise("Unexpected file extension for #{component_file}")
-                    end
-                    result
-                  }
+                  if Versionomy.parse(name) < Versionomy.parse('1.8.0')
+                    components_hash = get_components_hash_pre_vanagon_0_7()
+                  else
+                    components_hash = get_components_hash()
+                  end
                   [name, components_hash]
                 }
             ]
           end
           @data
+        end
+
+        # Before Vanagon 0.7, we have to actually just grep the component files. Boo.
+        def get_components_hash_pre_vanagon_0_7
+          @component_files.reduce(Hash.new) {|result, (component, config)|
+            component_file = PuppetReferences::AGENT_DIR + 'configs/components' + config
+            if component_file.extname == '.json'
+              result[component] = version_from_json(component_file)
+            elsif component_file.extname == '.rb'
+              result[component] = version_from_ruby(component_file)
+            else
+              raise("Unexpected file extension for #{component_file}")
+            end
+            result
+          }
+        end
+
+        def get_components_hash
+          temp_component_files = @component_files.merge( {'Ruby' => 'ruby-2.1.9.rb'} )
+          temp_component_files.reduce(Hash.new) {|result, (component, config)|
+            component_file = PuppetReferences::AGENT_DIR + 'configs/components' + config
+            if component_file.extname == '.json'
+              result[component] = version_from_json(component_file)
+            elsif component_file.extname == '.rb'
+              result[component] = version_from_ruby(component_file)
+            else
+              raise("Unexpected file extension for #{component_file}")
+            end
+            result
+          }
         end
 
         def version_from_json(file)
